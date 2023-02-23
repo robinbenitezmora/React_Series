c175f183f5011ac51ce1f1ebe33abb9879d74039
robinbenitezmora <robinbenitez1990@gmail.com>
2023-02-23 17:59:59 -0500

    Add button to the content

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Content = () => {
+  const [name, setName] = useState('Dave');
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
-    const names = ['Bom', 'Kevin', 'Dave'];
+    const names = ['Bob', 'Kevin', 'Dave'];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   }
+
+  const handleClick = () => {
+    setCount(count + 1)
+    console.log(count);
+  }
+
+  const handleClick2 = (e) => {
+    console.log(count);
+  }
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={handleClick2}>Click It</button>
     </main>
   )
 }
